3efdf6257e63406c0f2693f1780ef55a9de534a4
Cristian David Félix Paredes <david.df.01@hotmail.com>
2023-12-05 14:39:04 -0500

    app-employees ingresado

---
+++ src/app/employees/employee-form/employee-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-form.component.html',
+  styles: ``
+})
+export class EmployeeFormComponent {
+
+}
